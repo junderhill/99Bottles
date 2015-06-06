37eecb2fc26d51ffe1a3b781be1b76366c079c9e
Jason Underhill <jay.underhill@gmail.com>
2015-06-06 07:33:11 +0100

    First test passing, returns verse

--- BottlesOfBeer.Tests/TestBottlesOfBeer.cs
+++ BottlesOfBeer.Tests/TestBottlesOfBeer.cs
@@ -5,5 +5,16 @@ namespace BottlesOfBeer.Tests
     [TestClass]
     public class TestBottlesOfBeer
     {
+        [TestMethod]
+        public void TestThatGetVerseReturnsAnyVerse()
+        {
+            //arrange
+            var sut = new BottlesOfBeer();
+            var regex = new System.Text.RegularExpressions.Regex(@"^((\d)+|(No more))\sbottle(s)?\sof\sbeer\son\sthe\swall,\s((\d)+|(no more))\sbottle(s)?\sof\sbeer.\n(Take one down and pass it around|Go to the store and buy some more),\s((\d)+|(no more))\sbottle(s)?\sof\sbeer\son\sthe\swall\.");
+            //act
+            var result = sut.GetVerse(1);
+            //assert
+            Assert.IsTrue(regex.IsMatch(result));
+        }
     }
 }
--- BottlesOfBeer/BottlesOfBeer.cs
+++ BottlesOfBeer/BottlesOfBeer.cs
@@ -2,6 +2,9 @@
 {
     public class BottlesOfBeer
     {
-
+        public string GetVerse(int verseNo)
+        {
+            return "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.";
+        }
     }
 }
