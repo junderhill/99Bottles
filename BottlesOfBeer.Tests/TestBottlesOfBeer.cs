a9fc1be0c133e24bb7a296e49ddfeeb2497e5fd3
Jason Underhill <jay.underhill@gmail.com>
2015-06-06 08:23:03 +0100

    Second line of verse is returning correct string for most cases

--- BottlesOfBeer.Tests/TestBottlesOfBeer.cs
+++ BottlesOfBeer.Tests/TestBottlesOfBeer.cs
@@ -5,11 +5,16 @@ namespace BottlesOfBeer.Tests
     [TestClass]
     public class TestBottlesOfBeer
     {
+        private BottlesOfBeer sut;
+        [TestInitialize]
+        public void TestInit(){
+          sut = new BottlesOfBeer();
+        }
+
         [TestMethod]
         public void TestThatGetVerseReturnsAnyVerse()
         {
             //arrange
-            var sut = new BottlesOfBeer();
             var regex = new System.Text.RegularExpressions.Regex(@"^((\d)+|(No more))\sbottle(s)?\sof\sbeer\son\sthe\swall,\s((\d)+|(no more))\sbottle(s)?\sof\sbeer.\n(Take one down and pass it around|Go to the store and buy some more),\s((\d)+|(no more))\sbottle(s)?\sof\sbeer\son\sthe\swall\.");
             //act
             var result = sut.GetVerse(1);
@@ -19,13 +24,19 @@ namespace BottlesOfBeer.Tests
 
         [TestMethod]
         public void TestThatGetVerse1ReturnsCorrectVerse(){
-          //arrange
-          var sut = new BottlesOfBeer();
           //act
           var result = sut.GetVerse(1);
           //assert
-          string expected = @"99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.";
+          string expected = "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.";
           Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestThatGetVerse98Has1BottleLeftOnWall(){
+          //act
+          var result = sut.GetVerse(98);
+          //assert
+          Assert.IsTrue(result.Contains("Take one down and pass it around, 1 bottle of beer on the wall."));
         }
     }
 }
--- BottlesOfBeer/BottlesOfBeer.cs
+++ BottlesOfBeer/BottlesOfBeer.cs
@@ -1,10 +1,32 @@
-﻿namespace BottlesOfBeer
+﻿using System;
+using System.Text;
+namespace BottlesOfBeer
 {
     public class BottlesOfBeer
     {
         public string GetVerse(int verseNo)
         {
-            return "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.";
+            StringBuilder outputBuilder = new StringBuilder();
+            int beerQty = 100 - verseNo;
+          
+            outputBuilder.Append(String.Format("{0} {1} of beer on the wall, {0} {1} of beer.\n", beerQty, QuantifyBottle(beerQty).ToLower()));
+            if(beerQty == 0){
+              
+            }else{
+              outputBuilder.Append(String.Format("Take one down and pass it around, {0} {1} of beer on the wall.", beerQty-1, QuantifyBottle(beerQty-1).ToLower()));
+            }
+            return outputBuilder.ToString();
+        }
+
+        private string QuantifyBottle(int qty){
+          if(qty != 1)
+          {
+            return "Bottles";
+          }
+          else
+          {
+            return "Bottle";
+          }
         }
     }
 }
